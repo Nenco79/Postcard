1b84768fe0ec8c8047a0e17e1b5897340033f7d5
Stefano Burzacchi <stefano@MacBook-Pro-di-Stefano.local>
2014-09-22 19:27:29 +0200

    added dynamic name label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
-    
     @IBOutlet weak var enterNameTextField: UITextField!
-    
     @IBOutlet weak var enterMessageTextField: UITextField!
-    
     @IBOutlet weak var mailButton: UIButton!
     
     override func viewDidLoad() {
@@ -36,6 +34,10 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
         
         
